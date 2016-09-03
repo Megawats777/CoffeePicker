50df7c6aded0638b3bd1a0b4a6f964ff812debb6
Megawats777 <jedsemilla@gmail.com>
2016-09-02 20:29:37 -0400

    Finished basic layout of welcome screen

--- app/src/main/java/com/megawats/coffeepicker/MainActivity.java
+++ app/src/main/java/com/megawats/coffeepicker/MainActivity.java
@@ -3,6 +3,7 @@ package com.megawats.coffeepicker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,20 +13,12 @@ import java.text.NumberFormat;
 // Displays the screen to order coffee
 public class MainActivity extends AppCompatActivity {
 
-    // The number of coffee items ordered
-    private int orderedCoffeeAmount = 0;
+    // Opacity of the black coffee and iced coffee backgrounds
+    private int quantityBackgroundOpacity = 200;
 
-    // The price per coffee
-    private int pricePerCoffee = 5;
-
-    // The price of the user's order
-    private int totalOrderPrice = 0;
-
-    // Reference to the quantity text object
-    private TextView quantityText;
-
-    // Reference to the price text object
-    private TextView priceText;
+    // Reference to the black coffee and iced coffee backgrounds
+    private LinearLayout blackCoffeeAmountBackground;
+    private LinearLayout icedCoffeeAmountBackground;
 
     // Create the activity
     @Override
@@ -33,71 +26,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get the quantity text object
-        quantityText = (TextView) findViewById(R.id.quantityText);
+        // Get the black coffee and iced coffee backgrounds
+        blackCoffeeAmountBackground = (LinearLayout) findViewById(R.id.blackCoffeeAmountBackground);
+        icedCoffeeAmountBackground = (LinearLayout) findViewById(R.id.icedCoffeeAmountBackground);
 
-        // Reference to the price text object
-        priceText = (TextView) findViewById(R.id.priceText);
-    }
-
-    // Submit an order
-    public void submitOrder(View view)
-    {
-        // Calculate the price
-        calcalulatePrice();
-    }
-
-    // Increase the order amount
-    public void increaseOrderAmount(View view)
-    {
-        // Increase the ordered coffee amount
-        orderedCoffeeAmount++;
-
-        // Display the quantity amount
-        displayQuantityAmount(orderedCoffeeAmount);
-    }
-
-    // Decrease the order amount
-    public void decreaseOrderAmount(View view)
-    {
-        // If the ordered coffee amount is greater than 0 then decrease the ordered coffee amount
-        if (orderedCoffeeAmount > 0)
-        {
-            orderedCoffeeAmount--;
-
-            // Display the quantity amount
-            displayQuantityAmount(orderedCoffeeAmount);
-        }
-    }
-
-    // Calculate the price
-    private void calcalulatePrice()
-    {
-        // To get the total price take the number of coffee items ordered and multiply the by the pricePerCoffee value
-        totalOrderPrice = orderedCoffeeAmount * pricePerCoffee;
-
-        // Display the price
-        displayPrice(totalOrderPrice);
+        // Set the opacity of the black coffee and iced coffee backgrounds
+        blackCoffeeAmountBackground.getBackground().setAlpha(quantityBackgroundOpacity);
+        icedCoffeeAmountBackground.getBackground().setAlpha(quantityBackgroundOpacity);
     }
 
 
-    // Display the quantity amount
-    private void displayQuantityAmount(int amount)
-    {
-        // Convert the given amount to string
-        String newTextChar = Integer.toString(amount);
 
-        // Set the text for the quantity text
-        quantityText.setText(newTextChar);
-    }
-
-    // Display the price
-    private void displayPrice(int num)
-    {
-        // Convert the given number to string
-        String newPriceChar = Integer.toString(num);
-
-        // Set the text for the priceText
-        priceText.setText("$" + newPriceChar + ".00");
-    }
 }
