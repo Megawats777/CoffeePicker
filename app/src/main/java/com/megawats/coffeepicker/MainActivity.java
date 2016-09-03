c5c6013570d2c826c3f154323e4df16dabcc97c8
Megawats777 <jedsemilla@gmail.com>
2016-09-02 21:06:12 -0400

    You can now set the amount of coffee items you want, the price text updates whenever a new item is added, the app now stays in portrait mode throughout all of it's life

--- app/src/main/java/com/megawats/coffeepicker/MainActivity.java
+++ app/src/main/java/com/megawats/coffeepicker/MainActivity.java
@@ -1,17 +1,29 @@
 package com.megawats.coffeepicker;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.text.NumberFormat;
-
 // Displays the screen to order coffee
 public class MainActivity extends AppCompatActivity {
+
+    // The number of black coffee items ordered
+    private int orderedBlackCoffeeAmount = 0;
+
+    // The number of iced coffee items ordered
+    private int orderedIcedCoffeeAmount = 0;
+
+    // The price of each individual black coffee item
+    private int blackCoffeeItemPrice = 5;
+
+    // The price of each individual iced coffee
+    private int icedCoffeeItemPrice = 1;
+
+    // The total price of the order
+    private int totalOrderPrice = 0;
 
     // Opacity of the black coffee and iced coffee backgrounds
     private int quantityBackgroundOpacity = 200;
@@ -20,11 +32,25 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout blackCoffeeAmountBackground;
     private LinearLayout icedCoffeeAmountBackground;
 
+    // Reference to the black coffee and iced coffee amount text objects
+    private TextView blackCoffeeAmountText;
+    private TextView icedCoffeeAmountText;
+
+    // Get the order pice text object
+    private TextView orderPriceText;
+
     // Create the activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get to the black coffee and iced coffee amount text objects
+        blackCoffeeAmountText = (TextView) findViewById(R.id.blackCoffeeAmountText);
+        icedCoffeeAmountText = (TextView) findViewById(R.id.icedCoffeeAmountText);
+
+        // Get the order pice text object
+        orderPriceText = (TextView) findViewById(R.id.priceText);
 
         // Get the black coffee and iced coffee backgrounds
         blackCoffeeAmountBackground = (LinearLayout) findViewById(R.id.blackCoffeeAmountBackground);
@@ -35,6 +61,98 @@ public class MainActivity extends AppCompatActivity {
         icedCoffeeAmountBackground.getBackground().setAlpha(quantityBackgroundOpacity);
     }
 
+    /*--Black Coffee amount functions--*/
 
+    // Decrease the black coffee amount
+    public void decreaseBlackCoffeeAmount(View view)
+    {
+        // If the number of black coffee is greater than 0 then decrease the amount of black coffee
+        if (orderedBlackCoffeeAmount > 0)
+        {
+            orderedBlackCoffeeAmount--;
+
+            // Display the coffee amount
+            displayCoffeeAmount(blackCoffeeAmountText, orderedBlackCoffeeAmount);
+
+            // Calculate the total price
+            calculateTotalPrice();
+        }
+    }
+
+    // Increase the black coffee amount
+    public void increaseBlackCoffeeAmount(View view)
+    {
+        orderedBlackCoffeeAmount++;
+
+        // Display the coffee amount
+        displayCoffeeAmount(blackCoffeeAmountText, orderedBlackCoffeeAmount);
+
+        // Calculate the total price
+        calculateTotalPrice();
+    }
+
+    /*--Iced Coffee amount functions--*/
+
+    // Decrease the iced coffee amount
+    public void decreaseIcedCoffeeAmount(View view)
+    {
+        // If the number of iced coffee is greater than 0 then decrease the amount of iced coffee
+        if (orderedIcedCoffeeAmount > 0)
+        {
+            orderedIcedCoffeeAmount--;
+
+            // Display the coffee amount
+            displayCoffeeAmount(icedCoffeeAmountText, orderedIcedCoffeeAmount);
+
+            // Calculate the total price
+            calculateTotalPrice();
+        }
+    }
+
+    // Increase the iced coffee amount
+    public void increaseIcedCoffeeAmount(View view)
+    {
+        orderedIcedCoffeeAmount++;
+
+        // Display the coffee amount
+        displayCoffeeAmount(icedCoffeeAmountText, orderedIcedCoffeeAmount);
+
+        // Calculate the total price
+        calculateTotalPrice();
+    }
+
+    // Display the coffee amount
+    private void displayCoffeeAmount(TextView textView,int num)
+    {
+        // Convert the number to string
+        String newCharDisplay = Integer.toString(num);
+
+        textView.setText(newCharDisplay);
+    }
+
+
+    // Calculate the total price
+    private void calculateTotalPrice()
+    {
+        // The total price for all black coffee items ordered
+        int blackCoffeeTotalPrice = orderedBlackCoffeeAmount * blackCoffeeItemPrice;
+
+        // The total price for all iced coffee items ordered
+        int icedCoffeeTotalPrice = orderedIcedCoffeeAmount * icedCoffeeItemPrice;
+
+        // Add the two prices to make up the total order price
+        totalOrderPrice = blackCoffeeTotalPrice + icedCoffeeTotalPrice;
+
+        // Display the total price
+        displayTotalPriceText(totalOrderPrice);
+    }
+
+
+    // Display the total price
+    private void displayTotalPriceText(int num)
+    {
+        String displayedPrice = Integer.toString(num);
+        orderPriceText.setText("$" + displayedPrice + ".00");
+    }
 
 }
