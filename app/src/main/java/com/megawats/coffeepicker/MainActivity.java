978bc398fe755f11ae55dafa07375968698e8e20
Megawats777 <jedsemilla@gmail.com>
2016-09-05 15:31:39 -0400

    Added OrderInfo class

--- app/src/main/java/com/megawats/coffeepicker/MainActivity.java
+++ app/src/main/java/com/megawats/coffeepicker/MainActivity.java
@@ -9,20 +9,35 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 // Displays the screen to order coffee
 public class MainActivity extends AppCompatActivity {
 
     // The background of the checkout button
     RelativeLayout checkoutButtonBackground;
 
+    /*--Item text views--*/
+    private TextView blackCoffeeOrderAmountText;
+    private TextView icedCoffeeOrderAmountText;
+    private TextView waterOrderAmountText;
+    private TextView orangeJuiceOrderAmountText;
+
     // Create the activity
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Get the background of the checkout button
         checkoutButtonBackground  = (RelativeLayout) findViewById(R.id.checkoutButtonBackground);
+
+        // Get the order amount text views
+        blackCoffeeOrderAmountText = (TextView) findViewById(R.id.blackCoffeeOrderAmountText);
+        icedCoffeeOrderAmountText = (TextView) findViewById(R.id.icedCoffeeOrderAmountText);
+        waterOrderAmountText = (TextView) findViewById(R.id.waterOrderAmountText);
+        orangeJuiceOrderAmountText = (TextView) findViewById(R.id.orangeJuiceOrderAmountText);
 
         // Set the opacity of the checkout button background
         checkoutButtonBackground.getBackground().setAlpha(150);
---
+++ app/src/main/java/com/megawats/coffeepicker/OrderInfo.java
@@ -0,0 +1,26 @@
+package com.megawats.coffeepicker;
+
+/**
+ * Created by jedse on 2016-09-05.
+ */
+
+// Holds information for the user's order
+public class OrderInfo
+{
+    /*--The total order price--*/
+    public static int totalOrderPrice;
+
+    /*--The Price of the items--*/
+    public static int blackCoffeePrice = 5;
+    public static int icedCoffeePrice = 2;
+    public static int waterPrice = 1;
+    public static int orangeJuicePrice = 2;
+
+    /*--The number of orders per item and in total--*/
+    public static int totalOrderAmount = 0;
+    public static int blackCoffeeOrderAmount = 0;
+    public static int icedCoffeeOrderAmount = 0;
+    public static int waterOrderAmount = 0;
+    public static int orangeJuiceOrderAmountPrice = 0;
+}
+
