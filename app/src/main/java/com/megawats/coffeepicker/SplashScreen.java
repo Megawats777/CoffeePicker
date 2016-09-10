d1a9eb988d1e57a4c9440588c644f71766653075
Megawats777 <jedsemilla@gmail.com>
2016-09-10 13:34:41 -0400

    Made adjustments to manifest file

---
+++ app/src/main/java/com/megawats/coffeepicker/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.megawats.coffeepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Hide the action bar
+        getSupportActionBar().hide();
+    }
+}
